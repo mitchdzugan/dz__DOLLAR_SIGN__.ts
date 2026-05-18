0810b9e2f8f22947575f0bc505c68dfbeb913345
Mitch Dzugan <mitchdzugan@gmail.com>
2026-05-18 09:45:01 -0500

    rwse goodener

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export * from "./core.js";
-export * from "./rwse2.js";
+export * from "./rwse.js";
 export * as Id from "./id.js";
 export * as Inc from "./incremental.js";
 export * as Int from "./interrupt.js";
